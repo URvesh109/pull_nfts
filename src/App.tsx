562feffaf1e2f6be9c6fb49f48d17c40bb5a9e42
Urvesh Ghoderao <urveshghoderao@Urveshs-MacBook-Pro.local>
2022-03-13 10:26:46 +0530

    Prettier config added

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
 import * as React from "react";
 import AppNavigation from "./AppNavigation";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { clusterApiUrl } from "@solana/web3.js";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-ant-design";
 
 const App = () => {
-  const network = WalletAdapterNetwork.Devnet;
-  const endpoint = React.useMemo(() => clusterApiUrl(network), [network]);
-  const wallets = React.useMemo(
-    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
-    []
-  );
-  return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <AppNavigation />
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  );
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint = React.useMemo(() => clusterApiUrl(network), [network]);
+    const wallets = React.useMemo(
+        () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+        [],
+    );
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <AppNavigation />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
 };
 
 export default App;
--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -9,41 +9,41 @@ import { WalletMultiButton } from "@solana/wallet-adapter-ant-design";
 const { Header } = Layout;
 
 const AppName: React.CSSProperties = {
-  color: "ActiveBorder",
-  fontWeight: "bold",
-  fontFamily: "monospace",
-  fontSize: 20,
+    color: "ActiveBorder",
+    fontWeight: "bold",
+    fontFamily: "monospace",
+    fontSize: 20,
 };
 
 const HomeBubtton: React.CSSProperties = {
-  color: "ActiveBorder",
-  borderColor: "transparent",
-  borderRadius: 5,
-  fontFamily: "monospace",
-  ...AppName,
+    color: "ActiveBorder",
+    borderColor: "transparent",
+    borderRadius: 5,
+    fontFamily: "monospace",
+    ...AppName,
 };
 
 const AppBar: React.FC = () => {
-  const navigate = useNavigate();
-  return (
-    <Layout>
-      <Header style={HeaderStyle}>
-        <div style={RowStyle}>
-          <div style={AppName}>
-            <Space onClick={() => navigate("/")}>
-              <Button style={HomeBubtton}>Aplha Fusion</Button>
-            </Space>
-          </div>
-          <div>
-            <Space>
-              <WalletMultiButton />
-            </Space>
-          </div>
-        </div>
-      </Header>
-      <Outlet />
-    </Layout>
-  );
+    const navigate = useNavigate();
+    return (
+        <Layout>
+            <Header style={HeaderStyle}>
+                <div style={RowStyle}>
+                    <div style={AppName}>
+                        <Space onClick={() => navigate("/")}>
+                            <Button style={HomeBubtton}>Aplha Fusion</Button>
+                        </Space>
+                    </div>
+                    <div>
+                        <Space>
+                            <WalletMultiButton />
+                        </Space>
+                    </div>
+                </div>
+            </Header>
+            <Outlet />
+        </Layout>
+    );
 };
 
 export default AppBar;
